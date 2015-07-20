6c5addb7c8d0d2131e0e1d6f42cec813fb337ed6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-07-20 17:00:33 +0100

    configure logging

--- project.clj
+++ project.clj
@@ -6,6 +6,14 @@
   :dependencies [[manifold "0.1.1-alpha3"]
                  [org.onyxplatform/onyx "0.6.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+
+                                  [org.clojure/tools.logging "0.3.1"]
+                                  [ch.qos.logback/logback-classic "1.1.3"]
+                                  [org.slf4j/slf4j-api "1.7.12"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.12"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.12"]
+                                  [org.slf4j/jul-to-slf4j "1.7.12"]
+
                                   [yeller-timbre-appender "0.4.1"]
                                   [midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]]}})
