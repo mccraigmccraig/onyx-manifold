90d2d8dd6f546414816da3bae9609d66b366514d
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-07-20 18:08:25 +0100

    convert manifold source streams to core.async channels
    since alts!! is very useful for implementing timeouts,
    and manifold has no alts!! equivalents

--- src/onyx/plugin/manifold.clj
+++ src/onyx/plugin/manifold.clj
@@ -1,5 +1,6 @@
 (ns onyx.plugin.manifold
-  (:require [manifold.stream :refer [take! try-take! put!]]
+  (:require [clojure.core.async :refer [chan >!! <!! alts!! timeout]]
+            [manifold.stream :refer [connect take! put!]]
             [onyx.peer.pipeline-extensions :as p-ext]
             [onyx.static.default-vals :refer [defaults]]
             [taoensso.timbre :refer [debug info] :as timbre]))
@@ -7,10 +8,13 @@
 (defn inject-reader
   [event lifecycle]
   (assert (:manifold/stream event) ":manifold/stream not found - add it using a :before-task-start lifecycle")
-  {:manifold/pending-messages (atom {})
-   :manifold/drained? (atom false)
-   :manifold/retry-stream (manifold.stream/stream 10000)
-   :manifold/retry-count (atom 0)})
+  (let [stream-chan (chan)]
+    (connect (:manifold/stream event) stream-chan)
+    {:manifold/stream-chan stream-chan
+     :manifold/pending-messages (atom {})
+     :manifold/drained? (atom false)
+     :manifold/retry-chan (chan 10000)
+     :manifold/retry-count (atom 0)}))
 
 (defn log-retry-count
   [event lifecycle]
@@ -30,33 +34,27 @@
   {:lifecycle/before-task-start inject-writer})
 
 (defmethod p-ext/read-batch :manifold/read-from-stream
-  [{:keys [onyx.core/task-map manifold/stream manifold/retry-stream
+  [{:keys [onyx.core/task-map manifold/stream-chan manifold/retry-chan
            manifold/pending-messages manifold/drained?] :as event}]
   (let [pending (count @pending-messages)
         max-pending (or (:onyx/max-pending task-map) (:onyx/max-pending defaults))
         batch-size (:onyx/batch-size task-map)
         max-segments (min (- max-pending pending) batch-size)
         ms (or (:onyx/batch-timeout task-map) (:onyx/batch-timeout defaults))
-        t-timeout (+ ms (System/currentTimeMillis))
-        timeout-stream (manifold.stream/stream)
-        timeout-fn (fn [] (max 0 (- t-timeout (System/currentTimeMillis))))
+        timeout-ch (timeout ms)
         batch (if (pos? max-segments)
                 (loop [segments [] cnt 0]
                   (if (= cnt max-segments)
                     segments
-                    (let [message @(try-take! stream ::drained (timeout-fn) ::timeout)
-                          message (if (or (= ::drained message) (= ::timeout message))
-                                    @(try-take! retry-stream nil 0 nil)
-                                    message)]
-                      (if message
-                        (do
-                          (recur (conj segments
-                                       {:id (java.util.UUID/randomUUID)
-                                        :input :manifold
-                                        :message message})
-                                 (inc cnt)))
-                        segments))))
-                @(try-take! timeout-stream nil (timeout-fn) nil))]
+                    (if-let [message (first (alts!! [retry-chan stream-chan timeout-ch] :priority true))]
+                      (do
+                        (recur (conj segments
+                                     {:id (java.util.UUID/randomUUID)
+                                      :input :manifold
+                                      :message message})
+                               (inc cnt)))
+                      segments)))
+                (<!! timeout-ch))]
     (doseq [m batch]
       (swap! pending-messages assoc (:id m) (:message m)))
     (when (and (= 1 (count @pending-messages))
@@ -70,12 +68,12 @@
   (swap! pending-messages dissoc message-id))
 
 (defmethod p-ext/retry-message :manifold/read-from-stream
-  [{:keys [manifold/pending-messages manifold/retry-count manifold/retry-stream]} message-id]
+  [{:keys [manifold/pending-messages manifold/retry-count manifold/retry-chan]} message-id]
   (when-let [msg (get @pending-messages message-id)]
     (swap! pending-messages dissoc message-id)
     (when-not (= msg :done)
       (swap! retry-count inc))
-    (put! retry-stream msg)))
+    (>!! retry-chan msg)))
 
 (defmethod p-ext/pending? :manifold/read-from-stream
   [{:keys [manifold/pending-messages]} message-id]
